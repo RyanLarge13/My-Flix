3a9be98f442e5cd24dd7af25a9df41c3e46d7df5
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-10-21 20:53:37 -0700

    Adding innstatic database info due to issues saving data in MongoDB CLI

---
+++ movies.js
@@ -0,0 +1,191 @@
+const movie1 = {
+  Title: "Moana",
+  Description:
+    "An adventurous teenager sails out on a daring mission to save her people. During her journey, Moana meets the once-mighty demigod Maui, who guides her in her quest to become a master way-finder. Together they sail across the open ocean on an action-packed voyage, encountering enormous monsters and impossible odds. Along the way, Moana fulfills the ancient quest of her ancestors and discovers the one thing she always sought: her own identity.",
+  Genre: {
+    Name: "Adventure",
+    Description:
+      "Adventure film is a genre that revolves around the conquests and explorations of a protagonist. The purpose of the conquest can be to retrieve a person or treasure, but often the main focus is simply the pursuit of the unknown. These films generally take place in exotic locations and play on historical myths.",
+  },
+  Director: {
+    Name: "Ron Clements",
+    Bio: "Ron Clements was born on April 25, 1953 in Sioux City, Iowa, USA. He is a writer and director, known for Hercules (1997), Aladdin (1992) and The Princess and the Frog (2009). He has been married to Tamara Lee Glumace since February 25, 1989.",
+    Birth: "1953",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt3521164/mediaviewer/rm618728448/?ref_=tt_ov_i",
+  Featured: true,
+};
+
+const movie2 = {
+  Title: "127 Hours",
+  Description:
+    "While exploring a remote canyon in Utah, mountaineer and adventurer Aron Ralston (James Franco) becomes trapped when a boulder falls on his arm. Over the next five days, Ralston examines his life and considers his options, leading him to an agonizing choice: to amputate his arm so that he can extricate himself and try to make his way back to civilization or remain pinned to the canyon wall and likely die. Based on Ralston's book,",
+  Genre: {
+    Name: "Drama",
+    Description:
+      "The drama genre features stories with high stakes and many conflicts. They're plot-driven and demand that every character and scene move the story forward. Dramas follow a clearly defined narrative plot structure, portraying real-life scenarios or extreme situations with emotionally-driven characters.",
+  },
+  Director: {
+    Name: "Danny Boyle",
+    Bio: "Daniel Francis Boyle is an English director and producer. He is known for his work on films including Shallow Grave, Trainspotting and its sequel T2 Trainspotting, The Beach, 28 Days Later, Sunshine, Slumdog Millionaire, 127 Hours, Steve Jobs and Yesterday.",
+    Birth: "1956",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt1542344/mediaviewer/rm1114080768/?ref_=tt_ov_i",
+  Featured: false,
+};
+
+const movie3 = {
+  Title: "Titanic",
+  Description:
+    'James Cameron\'s "Titanic" is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built. She was the most luxurious liner of her era -- the "ship of dreams" -- which ultimately carried over 1,500 people to their death in the ice cold waters of the North Atlantic in the early hours of April 15, 1912.',
+  Genre: {
+    Name: "Drama",
+    Description:
+      "The drama genre features stories with high stakes and many conflicts. They're plot-driven and demand that every character and scene move the story forward. Dramas follow a clearly defined narrative plot structure, portraying real-life scenarios or extreme situations with emotionally-driven characters.",
+  },
+  Director: {
+    Name: "James Cameron",
+    Bio: "James Francis Cameron CC is a Canadian filmmaker and environmentalist. Best known for making science fiction and epic films, he first gained recognition for writing and directing The Terminator. He found further success with Aliens, The Abyss, Terminator 2: Judgment Day, and the action comedy True Lies.",
+    Birth: "1954",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt0120338/mediaviewer/rm2647458304/?ref_=tt_ov_i",
+  Featured: true,
+};
+
+const movie4 = {
+  Title: "Avatar",
+  Description:
+    "On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora. Jake Sully (Sam Worthington), a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na'vi woman (Zoe Saldana). As a bond with her grows, he is drawn into a battle for the survival of her world.",
+  Genre: {
+    Name: "Adventure",
+    Description:
+      "Adventure film is a genre that revolves around the conquests and explorations of a protagonist. The purpose of the conquest can be to retrieve a person or treasure, but often the main focus is simply the pursuit of the unknown. These films generally take place in exotic locations and play on historical myths.",
+  },
+  Director: {
+    Name: "James Cameron",
+    Bio: "James Francis Cameron CC is a Canadian filmmaker and environmentalist. Best known for making science fiction and epic films, he first gained recognition for writing and directing The Terminator. He found further success with Aliens, The Abyss, Terminator 2: Judgment Day, and the action comedy True Lies.",
+    Birth: "1954",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt0499549/mediaviewer/rm371527425/?ref_=tt_ov_i",
+  Featured: true,
+};
+
+const movie5 = {
+  Title: "The Matrix",
+  Description:
+    "Neo (Keanu Reeves) believes that Morpheus (Laurence Fishburne), an elusive figure considered to be the most dangerous man alive, can answer his question -- What is the Matrix? Neo is contacted by Trinity (Carrie-Anne Moss), a beautiful stranger who leads him into an underworld where he meets Morpheus. They fight a brutal battle for their lives against a cadre of viciously intelligent secret agents. It is a truth that could cost Neo something more precious than his life.",
+  Genre: {
+    Name: "Action",
+    Description:
+      "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.",
+  },
+  Director: {
+    Name: "Lana Wachowski",
+    Bio: "Lana Wachowski (born June 21, 1965, formerly known as Larry Wachowski) and Lilly Wachowski (born December 29, 1967, formerly known as Andy Wachowski) are American film and television directors, writers and producers.The sisters are both trans women.",
+    Birth: "1965",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt0133093/mediaviewer/rm525547776/?ref_=tt_ov_i",
+  Featured: true,
+};
+
+const movie6 = {
+  Title: "The Proposal",
+  Description:
+    "Faced with deportation to her native Canada, high-powered book editor Margaret Tate (Sandra Bullock) says she's engaged to marry Andrew Paxton (Ryan Reynolds), her hapless assistant. Andrew agrees to the charade, but imposes a few conditions of his own, including flying to Alaska to meet his eccentric family. With a suspicious immigration official always lurking nearby, Margaret and Andrew must stick to their wedding plan despite numerous mishaps.",
+  Genre: {
+    Name: "Comedy",
+    Description:
+      "A comedy film is a category of film which emphasizes humor. These films are designed to make the audience laugh through amusement. Films in this style traditionally have a happy ending (black comedy being an exception). One of the oldest genres in film—and derived from the classical comedy in theatre. ",
+  },
+  Director: {
+    Name: "Anne Fletcher",
+    Bio: "Anne Fletcher is an American choreographer, film director, dancer and actress. She directed the films Step Up, 27 Dresses and The Proposal.",
+    Birth: "1966",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt1041829/mediaviewer/rm2227985664/?ref_=tt_ov_i",
+  Featured: false,
+};
+
+const movie7 = {
+  Title: "Superman",
+  Description:
+    "Just before the destruction of the planet Krypton, scientist Jor-El (Marlon Brando) sends his infant son Kal-El on a spaceship to Earth. Raised by kindly farmers Jonathan (Glenn Ford) and Martha Kent (Phyllis Thaxter), young Clark (Christopher Reeve) discovers the source of his superhuman powers and moves to Metropolis to fight evil. As Superman, he battles the villainous Lex Luthor (Gene Hackman), while, as novice reporter Clark Kent, he attempts to woo co-worker Lois Lane (Margot Kidder).",
+  Genre: {
+    Name: "Action",
+    Description:
+      "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.",
+  },
+  Director: {
+    Name: "Richard Donner",
+    Bio: "Richard Donner was an American filmmaker whose notable works included some of the most financially-successful films during the New Hollywood era. According to film historian Michael Barson, Donner was one of Hollywood's most reliable makers of action blockbusters.",
+    Birth: "1930",
+    Death: "2021",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt0078346/mediaviewer/rm4053027072/?ref_=tt_ov_i",
+  Featured: false,
+};
+
+const movie8 = {
+  Title: "Slumdog Millionaire",
+  Description:
+    'As 18-year-old Jamal Malik (Dev Patel) answers questions on the Indian version of "Who Wants to Be a Millionaire," flashbacks show how he got there. Part of a stable of young thieves after their mother dies, Jamal and his brother, Salim, survive on the streets of Mumbai. Salim finds the life of crime agreeable, but Jamal scrapes by with small jobs until landing a spot on the game show.',
+  Genre: {
+    Name: "Drama",
+    Description:
+      "The drama genre features stories with high stakes and many conflicts. They're plot-driven and demand that every character and scene move the story forward. Dramas follow a clearly defined narrative plot structure, portraying real-life scenarios or extreme situations with emotionally-driven characters.",
+  },
+  Director: {
+    Name: "Danny Boyle",
+    Bio: "Daniel Francis Boyle is an English director and producer. He is known for his work on films including Shallow Grave, Trainspotting and its sequel T2 Trainspotting, The Beach, 28 Days Later, Sunshine, Slumdog Millionaire, 127 Hours, Steve Jobs and Yesterday.",
+    Birth: "1956",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt1010048/mediaviewer/rm2952200704/?ref_=tt_ov_i",
+  Featured: false,
+};
+
+const movie9 = {
+  Title: "Mary Poppins",
+  Description:
+    "When Jane (Karen Dotrice) and Michael (Matthew Garber), the children of the wealthy and uptight Banks family, are faced with the prospect of a new nanny, they are pleasantly surprised by the arrival of the magical Mary Poppins (Julie Andrews). Embarking on a series of fantastical adventures with Mary and her Cockney performer friend, Bert (Dick Van Dyke), the siblings try to pass on some of their nanny's sunny attitude to their preoccupied parents (David Tomlinson, Glynis Johns).",
+  Genre: {
+    Name: "Musical",
+    Description:
+      "Musical film is a film genre in which songs by the characters are interwoven into the narrative, sometimes accompanied by dancing. The songs usually advance the plot or develop the film's characters, but in some cases, they serve merely as breaks in the storyline, often as elaborate production numbers.",
+  },
+  Director: {
+    Name: "Robert Stevenson",
+    Bio: "Robert Edward Stevenson was an English film screenwriter, director and actor. After directing a number of British films, including King Solomon's Mines, he was contracted by David O. Selznick and moved to Hollywood, but was loaned to other studios, directing Jane Eyre.",
+    Birth: "1905",
+    Death: "1986",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt0058331/mediaviewer/rm2777561856/?ref_=tt_ov_i",
+  Featured: false,
+};
+
+const movie10 = {
+  Title: "Star Wars Revenge of the Sith",
+  Description:
+    "It has been three years since the Clone Wars began. Jedi Master Obi-Wan Kenobi (Ewan McGregor) and Jedi Knight Anakin Skywalker (Hayden Christensen) rescue Chancellor Palpatine (Ian McDiarmid) from General Grievous, the commander of the droid armies, but Grievous escapes. Suspicions are raised within the Jedi Council concerning Chancellor Palpatine, with whom Anakin has formed a bond. Asked to spy on the chancellor, and full of bitterness toward the Jedi Council, Anakin embraces the Dark Side.",
+  Genre: {
+    Name: "Adventure",
+    Description:
+      "Adventure film is a genre that revolves around the conquests and explorations of a protagonist. The purpose of the conquest can be to retrieve a person or treasure, but often the main focus is simply the pursuit of the unknown. These films generally take place in exotic locations and play on historical myths.",
+  },
+  Director: {
+    Name: "George Lucas",
+    Bio: "George Walton Lucas Jr. is an American filmmaker. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, LucasArts, and Industrial Light & Magic. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012.",
+    Birth: "1944",
+  },
+  ImageUrl:
+    "https://m.imdb.com/title/tt0121766/mediaviewer/rm4094016256/?ref_=tt_ov_i",
+  Featured: false,
+};
